a73043fec6123a29f0dec996cb69e19f23db1fd1
Drew <hatfielddr>
2020-12-31 03:28:40 -0500

    Inventory and basic items

--- src/map.rs
+++ src/map.rs
@@ -5,9 +5,9 @@ use specs::prelude::*;
 
 use super::Rect;
 
-const MAPWIDTH: usize = 80;
-const MAPHEIGHT: usize = 50;
-const MAPCOUNT: usize = MAPHEIGHT * MAPWIDTH;
+pub const MAPWIDTH: usize = 80;
+pub const MAPHEIGHT: usize = 43;
+pub const MAPCOUNT: usize = MAPHEIGHT * MAPWIDTH;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum TileType {
@@ -134,6 +134,7 @@ impl BaseMap for Map {
     fn is_opaque(&self, idx: usize) -> bool {
         self.tiles[idx] == TileType::Wall
     }
+
 
     fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
         let w = self.width as usize;
